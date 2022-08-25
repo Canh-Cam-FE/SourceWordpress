ba11b25d8a9bedb82940d60e73cc2363228c7c7d
Quang Nguyên <55746340+QuangNguyen1607@users.noreply.github.com>
2022-08-25 13:42:28 +0700

    upload

--- CanhCamTheme/inc/function-field.php
+++ CanhCamTheme/inc/function-field.php
@@ -1,31 +1,4 @@
 <?php
-function add_field_banner()
-{
-	acf_add_local_field_group(array(
-		'key' => 'banner',
-		'title' => 'Banner: Select Page',
-		'fields' => array(),
-		'location' => array(
-			array(
-				array(
-					'param' => 'post_type',
-					'operator' => '==',
-					'value' => 'banner',
-				),
-			),
-		),
-	));
-	acf_add_local_field(array(
-		'key' => 'banner_select_page',
-		'label' => 'Chọn trang hiển thị',
-		'name' => 'Chọn trang hiển thị',
-		'type' => 'post_object',
-		'post_type' => 'page',
-		'parent' => 'banner',
-		'return_format' => 'id',
-	));
-}
-add_action('acf/init', 'add_field_banner');
 // Custom field class for page
 function add_field_custom_class_body()
 {
@@ -59,3 +32,48 @@ function add_field_custom_class_body()
 	));
 }
 add_action('acf/init', 'add_field_custom_class_body');
+
+//
+
+function add_field_select_banner()
+{
+	acf_add_local_field_group(array(
+		'key' => 'select_banner',
+		'title' => 'Banner: Select Page',
+		'fields' => array(),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'page',
+				),
+			),
+			array(
+				array(
+					'param' => 'taxonomy',
+					'operator' => '==',
+					'value' => 'category',
+				),
+			),
+			// Thêm taxonomy ở dưới
+			// array(
+			// 	array(
+			// 		'param' => 'taxonomy',
+			// 		'operator' => '==',
+			// 		'value' => 'danh-muc-san-pham'
+			// 	)
+			// )
+		),
+	));
+	acf_add_local_field(array(
+		'key' => 'banner_select_page',
+		'label' => 'Chọn banner hiển thị',
+		'name' => 'Chọn banner hiển thị',
+		'type' => 'post_object',
+		'post_type' => 'banner',
+		'multiple' => 1,
+		'parent' => 'select_banner',
+	));
+}
+add_action('acf/init', 'add_field_select_banner');
