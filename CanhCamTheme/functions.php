355239ade7a28fa09387e1aff89e4a23cb51791e
Quang Nguyên <55746340+QuangNguyen1607@users.noreply.github.com>
2023-07-07 09:19:38 +0700

    Upload

--- CanhCamTheme/functions.php
+++ CanhCamTheme/functions.php
@@ -1,9 +1,7 @@
 <?php
 define('GENERATE_VERSION', '1.1.0');
-if (class_exists('CanhCam_Licsence_Class')) {
-	require get_template_directory() . '/inc/function-root.php';
-	require get_template_directory() . '/inc/function-custom.php';
-	require get_template_directory() . '/inc/function-field.php';
-	require get_template_directory() . '/inc/function-setup.php';
-	require get_template_directory() . '/inc/function-pagination.php';
-}
+require get_template_directory() . '/inc/function-root.php';
+require get_template_directory() . '/inc/function-custom.php';
+require get_template_directory() . '/inc/function-field.php';
+require get_template_directory() . '/inc/function-setup.php';
+require get_template_directory() . '/inc/function-pagination.php';
