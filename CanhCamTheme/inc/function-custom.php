5e6c81f6ee1b50044687837ab185a25893504085
Thanh Lâm <70793594+lamzaaa@users.noreply.github.com>
2023-09-19 11:23:40 +0700

    update function render img(width/height/alt/lazyload)

--- CanhCamTheme/inc/function-custom.php
+++ CanhCamTheme/inc/function-custom.php
@@ -31,4 +31,43 @@ function empty_content($str)
 	return trim(str_replace('&nbsp;', '', strip_tags($str, '<img>'))) == '';
 }
 
+function custom_get_post_thumbnail($post_id, $size = 'full', $attr = '')
+{
+    if (is_array($post_id))
+        $post_id = $post_id["ID"];
+    $post_thumbnail_id = get_post_thumbnail_id($post_id);
+    $image_attributes = wp_get_attachment_image_src($post_thumbnail_id, $size);
+    $alt_text = get_post_meta($post_thumbnail_id, '_wp_attachment_image_alt', true);
+
+    if ($image_attributes) {
+        $html = "<img width='" . $image_attributes[1] . "' height='" . $image_attributes[2] . "' data-src='" . esc_url($image_attributes[0]) . "' class='lozad'";
+        if (empty($alt_text)) {
+            $html .= 'alt="' . get_wp_title_rss('') . '"';
+        } else {
+            $html .= ' alt="' . esc_attr($alt_text) . '"';
+        }
+        $html .= ' />';
+
+        return $html;
+    }
+}
+
+function custom_lozad_image($attachment_id, $size = 'full', $icon = false, $attr = '')
+{
+    if (is_array($attachment_id))
+        $attachment_id = $attachment_id["ID"];
+    $image_attributes = wp_get_attachment_image_src($attachment_id, $size);
+    $alt_text = get_post_meta($attachment_id, '_wp_attachment_image_alt', true);
+    if ($image_attributes) {
+        $html = "<img width='" . $image_attributes[1] . "' height='" . $image_attributes[2] . "' data-src='" . esc_url($image_attributes[0]) . "' class='lozad'";
+        if (empty($alt_text)) {
+            $html .= 'alt="' . get_wp_title_rss('') . '"';
+        } else {
+            $html .= ' alt="' . esc_attr($alt_text) . '"';
+        }
+        $html .= ' />';
+        return $html;
+    }
+}
+
 ?>
